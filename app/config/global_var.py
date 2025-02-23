783c10422eb62d70edad220ce68896869fe5e478
yjh <483737482@qq.com>
2025-02-23 19:36:06 +0800

    算法优化-2.0

---
+++ app/config/global_var.py
@@ -0,0 +1,5 @@
+main_uuid = "main_uuid"
+w = 0.0
+h = 0.0
+x = 0.0
+y = 0.0
---
+++ app/services/layout_service/test/test_ssa.py
@@ -0,0 +1,10 @@
+from app.services.layout_service.uniform.uniform_layout import uniform_layout
+
+
+def test_ssa():
+    uniform_layout()
+
+
+if __name__ == '__main__':
+    test_ssa()
+    print('ok')
